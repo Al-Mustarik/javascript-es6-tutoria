8ec22c34896d668bf44a213e2eec00b6aea38b73
Al Mustarik <almustarik@gmail.com>
2020-10-26 19:07:33 +0600

    understanding default function argument

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ JAVASCRIPT BASIC
 1. var, let , const
 2. template string
 3. template tagging
+4. Default function argument
--- main.js
+++ main.js
@@ -1,37 +1,51 @@
-const firstName = 'Al';
-const lastName = 'Mustarik';
-const age = 25 ;
-const profession = 'Data Scientist';
+// const firstName = 'Al';
+// const lastName = 'Mustarik';
+// const age = 25 ;
+// const profession = 'Data Scientist';
 
-// function myBio(){
-//     return 'my name is ' + firstName +' '+ lastName + 'and I am ' + age + 'years old ' + 'and I am a professional ' + profession ;
+// // function myBio(){
+// //     return 'my name is ' + firstName +' '+ lastName + 'and I am ' + age + 'years old ' + 'and I am a professional ' + profession ;
+// // }
+
+// // function myBio(){
+//     // TEMPLATE STRING
+// //     return `My name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} and my age is ${age -1} years old and I'm a professional ${profession}`;
+// // }
+
+// // console.log(myBio());
+
+// // TEMPLATE TAGGING
+
+// function modify(strings, ...values ){
+//     // console.log(strings, values);
+//     let str = "";
+//     strings.forEach(function(string, i){
+//         str += `${string} <b> ${values[i] || "" } </b>`;
+//     })
+//     return str;
 // }
 
-// function myBio(){
-    // TEMPLATE STRING
-//     return `My name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} and my age is ${age -1} years old and I'm a professional ${profession}`;
+// const html = modify`
+
+//     <h1>Header</h1>
+//     <p> I am a paragraph </p>
+//     <p>My name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} 
+//     and my age is ${age -1} years old and I'm a professional ${profession}</p>
+
+// `;
+
+// document.body.innerHTML = html;
+
+//number function
+//Function statement
+// function add(num1, num2){
+//     return num1 + num2 ;
 // }
 
-// console.log(myBio());
 
-// TEMPLATE TAGGING
+//Function expression
 
-function modify(strings, ...values ){
-    // console.log(strings, values);
-    let str = "";
-    strings.forEach(function(string, i){
-        str += `${string} ${values[i] || "" }`;
-    })
-    return str;
+const add = function(num1 = 2, num2){
+    return num1 + num2;
 }
-
-const html = modify`
-
-    <h1>Header</h1>
-    <p> I am a paragraph </p>
-    <p>My name is ${firstName.toUpperCase()} ${lastName.toUpperCase()} 
-    and my age is ${age -1} years old and I'm a professional ${profession}</p>
-
-`;
-
-document.body.innerHTML = html;
+console.log(add(undefined, 2));
