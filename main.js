3541a564df0f3169a33c767599b57621aab637a1
Al Mustarik <almustarik@gmail.com>
2020-10-27 01:07:21 +0600

    For of Loop

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
-// //constructor way
-// // new keyword to declare object
+//for loop
 
-// const myObj = new Object();
+const lang = ['html', 'css', 'js'];
 
-// const programming = {
-//     first: "Javascript",
-//     second: "php",
-//     third: "python",
-//     common: ['Html', 'css', 'basic javascript'],
-//     choosenLanguage: function(){
-//         return `My first priority-${this.first}. My second priority-${this.second}`;
+//simple for loop
+// for(let i=0; i < lang.length; i++){
+//     if(lang[i] === 'css'){
+//         continue;
 //     }
+//     console.log(lang[i]);
 // }
-// console.log(programming.first);
-// console.log(programming.second);
-// console.log(programming.choosenLanguage());
 
-function bio({firstName,lastName,choosenLang}){
-    return{
-        firstName, //if property and value name both are similar then we don't need to assign
-        lastName: lastName,
-        common: ['Html', 'css', 'basic javascript'],
-        choosenLanguage(){
-            return `My first name- ${this.firstName}. My last name- ${this.lastName} and I prefer ${choosenLang}`;
-        }
+//forEach loop
+
+// lang.forEach((eachlang,i) => { //continue or break don't work
+//     // if (eachlang === 'css'){
+//     //     continue;
+//     // }
+//     console.log(i, eachlang);
+// })
+
+
+//forin loop (resulting Index)
+// const obj = {
+//     firstName: 'Al',
+//     lastName: 'Mustarik'
+// }
+
+//Array Prototype
+// Array.prototype.myFunc = () => { //Access Prototype
+//     console.log('Hello...');
+// }
+// lang.last = 'Python';
+
+// for (const name in lang) {
+//     // console.log(name);
+//     console.log(lang[name]);
+// }
+
+
+//for of loop
+
+for (let [i, myLang] of lang.entries()){ //not looping object
+    if (myLang === 'css'){
+        continue;
     }
+    console.log(i, myLang);
 }
-
-const myBio = bio({
-    firstName: 'Al',
-    lastName: 'Mustarik',
-    choosenLang: 'Javascript'
-});
-console.log(myBio.firstName);
-console.log(myBio.lastName);
-console.log(myBio.choosenLanguage());
