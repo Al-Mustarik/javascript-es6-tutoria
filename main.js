9f9d519e5d681d0400cbf1e633fc95ef5db1f012
Al Mustarik <almustarik@gmail.com>
2020-10-26 23:37:26 +0600

    destructuring simple project for better understanding

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-//array destructuring
+//Working with function (destructuring mind)
+
+function myBio({lastName, firstName, age, email, address}){
+    return `${firstName} ${lastName} is ${age} years old recently i stay at ${address} and my email address is  ${email} `;
+}
+
+console.log(
+    myBio({
+    firstName: "Al",
+    lastName: "Mustarik",
+    age: 25,
+    address: "22/C/2, Lake City",
+    email: "mustarik.al@outlook.com"
+    })
+);
 
 
-// const myProfile = ["Al", "Mustarik", 23, "Data Scientist"];
-// const [, , age,profession] = myProfile;
-// console.log(profession);
-
-
-//Object destructuring
-
-
-const post = {
-    id: 1,
-    title: "sample title",
-    body: "sample body",
-    allowComments: true,
-    share:{
-        facebook: true,
-        twitter: true,
-        linkdid: false
+const comments = [
+    {
+        id: 10,
+        body: "first comment"
+    },
+    {
+        id: 2,
+        body: "second comment"
+    },
+    {
+        id: 4,
+        body: "dump comment"
+    },
+    {
+        id: 3,
+        body: "correct comment"
     }
-};
+]
 
-// const {title, allowComments} = post;
-const {facebook} = post.share;
-console.log(facebook);
+const badComment = comments.findIndex(comment => comment.id === 4);
+console.log(badComment);
+const choosenComment = [...comments.slice(0, badComment), ...comments.slice(badComment + 1)];
+console.log(choosenComment)
