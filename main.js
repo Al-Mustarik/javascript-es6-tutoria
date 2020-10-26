2d4db38bd39e51bfece9518859acef560ce27c22
Al Mustarik <almustarik@gmail.com>
2020-10-27 00:42:34 +0600

    object literal basic

--- main.js
+++ main.js
@@ -1,40 +1,37 @@
-//Working with function (destructuring mind)
+// //constructor way
+// // new keyword to declare object
 
-function myBio({lastName, firstName, age, email, address}){
-    return `${firstName} ${lastName} is ${age} years old recently i stay at ${address} and my email address is  ${email} `;
+// const myObj = new Object();
+
+// const programming = {
+//     first: "Javascript",
+//     second: "php",
+//     third: "python",
+//     common: ['Html', 'css', 'basic javascript'],
+//     choosenLanguage: function(){
+//         return `My first priority-${this.first}. My second priority-${this.second}`;
+//     }
+// }
+// console.log(programming.first);
+// console.log(programming.second);
+// console.log(programming.choosenLanguage());
+
+function bio({firstName,lastName,choosenLang}){
+    return{
+        firstName, //if property and value name both are similar then we don't need to assign
+        lastName: lastName,
+        common: ['Html', 'css', 'basic javascript'],
+        choosenLanguage(){
+            return `My first name- ${this.firstName}. My last name- ${this.lastName} and I prefer ${choosenLang}`;
+        }
+    }
 }
 
-console.log(
-    myBio({
-    firstName: "Al",
-    lastName: "Mustarik",
-    age: 25,
-    address: "22/C/2, Lake City",
-    email: "mustarik.al@outlook.com"
-    })
-);
-
-
-const comments = [
-    {
-        id: 10,
-        body: "first comment"
-    },
-    {
-        id: 2,
-        body: "second comment"
-    },
-    {
-        id: 4,
-        body: "dump comment"
-    },
-    {
-        id: 3,
-        body: "correct comment"
-    }
-]
-
-const badComment = comments.findIndex(comment => comment.id === 4);
-console.log(badComment);
-const choosenComment = [...comments.slice(0, badComment), ...comments.slice(badComment + 1)];
-console.log(choosenComment)
+const myBio = bio({
+    firstName: 'Al',
+    lastName: 'Mustarik',
+    choosenLang: 'Javascript'
+});
+console.log(myBio.firstName);
+console.log(myBio.lastName);
+console.log(myBio.choosenLanguage());
