75fcc67e686ff75b5124e14e5571c22b400a8117
Al Mustarik <almustarik@gmail.com>
2020-10-27 21:05:02 +0600

    es6-promise

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 //Fetch API
-fetch('https://jsonplaceholder.typicode.com/comments')
-.then(data => data.json())
-.then(data => console.log(data))
-.catch(e => console.log(e));
+// fetch('https://jsonplaceholder.typicode.com/comments')
+// .then(data => data.json())
+// .then(data => console.log(data))
+// .catch(e => console.log(e));
 
 //Behind the scene working with Promise
+
+// function logData(data){
+    
+//     setTimeout(() => {
+//         console.log(1);
+//         setTimeout(() => {
+//             console.log(2);
+//         },2000);
+//     },2000);   
+// }
+
+// logData();
+
+const logDataP = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(Error('This is error'));
+        },2000 );
+    })
+    
+logDataP.then(data => {
+    console.log(data);
+}).catch(e => console.log(e));
